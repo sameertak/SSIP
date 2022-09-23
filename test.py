dfa402c013b886fac14946af65f01f08e997d832
taksa <taksamir08@gmail.com>
2022-09-23 23:02:52 +0530

    'checking'

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-data = {
-    "res1" : "A",
-    "res2" : "B",
-    "res3" : "asidygfasiudhashdosah dashdiuasndiasyduasdas dsibsiducbds said ci iasuasniuasbncbasij casi ciasb sa"
-}
-answer = requests.post('https://otplogin.pythonanywhere.com/feedback/form/', json=data)

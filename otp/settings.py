5460fdfad06ec90420339b2bd12635eda063e8a3
taksa <taksamir08@gmail.com>
2022-09-24 00:22:35 +0530

    Updating Database

--- otp/settings.py
+++ otp/settings.py
@@ -86,21 +86,16 @@ WSGI_APPLICATION = 'otp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-import dj_database_url
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
-# default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-#
-# DATABASE = {'default': config('DATABASE_URL', default = default_dburl, cast=parse)}
+DATABASE = {'default': config('DATABASE_URL', default = default_dburl, cast=parse)}
 
 # DEFAULT_AUTO_FIELD defined because  verification.phoneModel Auto-created primary key used 
 # when not defining a primary key type, by default 'django.db.models.AutoField'.

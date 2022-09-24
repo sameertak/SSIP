f43e385d00bbe02f4828591af97c4470a9b1ecd0
taksa <taksamir08@gmail.com>
2022-09-24 19:47:58 +0530

    Updated Twilio Token

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -5,6 +6,7 @@ from .serializers import FeedbackSerializers
 from .models import responseModel
 
 class form(APIView):
+    permission_classes = (IsAuthenticated,)
     def post(self, request):
         serializer = FeedbackSerializers(data=request.data)
         if serializer.is_valid():

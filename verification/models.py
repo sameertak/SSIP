11d36a3d04e62f3807e51efadb7ed80f3912c4c7
taksa <taksamir08@gmail.com>
2022-09-24 11:03:47 +0530

    Updating Model

--- verification/models.py
+++ verification/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # this model Stores the data of the Phones Verified
 class phoneModel(models.Model):
-    Mobile = models.IntegerField(blank=False)
+    Mobile = models.CharField(max_length=13, blank=False)
     isVerified = models.BooleanField(blank=False, default=False)
     counter = models.IntegerField(default=0, blank=False)   # For HOTP Verification
 

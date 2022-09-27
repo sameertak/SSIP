ce19eb186250157e1449ba0f0d1316e51a11725e
taksa <taksamir08@gmail.com>
2022-09-28 00:01:05 +0530

    Updated Cors

--- otp/settings.py
+++ otp/settings.py
@@ -65,9 +65,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:5174',
-)
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'otp.urls'
 

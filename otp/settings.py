19d765f10f90f8a33ed0c8b212169690f6af52d4
taksa <taksamir08@gmail.com>
2022-09-28 00:12:15 +0530

    Updated Cors

--- otp/settings.py
+++ otp/settings.py
@@ -79,7 +79,7 @@ CORS_ALLOW_HEADERS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:5174", "http://127.0.0.1:5174"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5174", "http://127.0.0.1:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5173"]
 
 ROOT_URLCONF = 'otp.urls'
 

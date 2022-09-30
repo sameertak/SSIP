cbf5c1f17da8526cde90cd5c9f20947ecc4c964a
taksa <taksamir08@gmail.com>
2022-09-30 15:38:23 +0530

    updated model

--- feedback/forms.py
+++ feedback/forms.py
@@ -4,6 +4,7 @@ from django import forms
 
 
 class FeedbackForm(forms.Form):
-	res1 = forms.CharField(max_length = 5)
-	res2 = forms.CharField(max_length = 5)
+	res1 = forms.CharField(max_length = 50)
+	res2 = forms.CharField(max_length = 50)
 	res3 = forms.CharField(widget = forms.Textarea, max_length = 200)
+	res4 = forms.CharField(max_length = 1)
--- feedback/serializers.py
+++ feedback/serializers.py
@@ -2,9 +2,10 @@ from rest_framework import serializers
 from .models import responseModel
 
 class FeedbackSerializers(serializers.ModelSerializer):
-    res1 = serializers.CharField(max_length=1)
-    res2 = serializers.CharField(max_length=1)
-    res3 = serializers.CharField(max_length=200)
+    res1 = serializers.CharField(max_length=50)
+    res2 = serializers.CharField(max_length=50)
+    res3 = serializers.CharField(max_length=300)
+    res4 = serializers.CharField(max_length=1)
 
     class Meta:
         model = responseModel

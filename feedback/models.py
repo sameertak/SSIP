4f6088519da0ca298a7f372cc7c7c68692c623f3
taksa <taksamir08@gmail.com>
2022-09-30 15:51:32 +0530

    updated model

--- feedback/models.py
+++ feedback/models.py
@@ -14,9 +14,9 @@ class responseModel(models.Model):
     res2 = models.CharField(max_length=50, blank=False)
     res3 = models.CharField(max_length=300)
     res4 = models.CharField(max_length=1, blank=False)
-    ip_address = models.CharField(default='0.0.0.0', max_length=30)
-    city = models.CharField(default='NaN', max_length=15, null=True, blank=True)
-    lat_lng = models.CharField(default='NaN', max_length=30, null=True, blank=True)
+    ip_address = models.CharField(max_length=30)
+    city = models.CharField(max_length=15, null=True, blank=True)
+    lat_lng = models.CharField(max_length=30, null=True, blank=True)
 
     def __str__(self):
         return str(self.res_id)

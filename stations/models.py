ec95347f720abdf87d2bfd11246f1bd7bd0f3153
taksa <taksamir08@gmail.com>
2022-10-03 01:16:02 +0530

    Initialized Stations Application

--- stations/models.py
+++ stations/models.py
@@ -6,3 +6,10 @@ from django.db import models
 class Station(models.Model):
     station_id = models.CharField(max_length=10)
     station_name = models.CharField(max_length=25)
+    district = models.CharField(max_length=15)
+    subdivision = models.CharField(max_length=10)
+    contact = models.CharField(max_length=12)
+    pincode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self(self.station_name)
--- stations/urls.py
+++ stations/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
-from .views import Stations
-
-urlpatterns = [
-    path('^get_data/(?P<username>.+)/$', Stations.as_view(), name="Filter Parameters"),
-]
+# from django.urls import path, include
+# from .views import Stations
+#
+# urlpatterns = [
+#     path('^get_data/(?P<username>.+)/$', Stations.as_view(), name="Filter Parameters"),
+# ]

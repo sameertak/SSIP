2299262491f17d381aab230dc8311c82ca1df54d
taksa <taksamir08@gmail.com>
2022-10-03 01:58:14 +0530

    Updated Model

--- stations/models.py
+++ stations/models.py
@@ -8,6 +8,7 @@ class Station(models.Model):
     station_name = models.CharField(max_length=25)
     district = models.CharField(max_length=15)
     subdivision = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
     contact = models.CharField(max_length=12)
     pincode = models.CharField(max_length=6)
 

d846e4eaa82c94826c3df5633640ed21ba68bf7c
taksa <taksamir08@gmail.com>
2022-10-03 14:07:31 +0530

    Updated Model

--- stations/models.py
+++ stations/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 
 
 class Station(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     station_id = models.CharField(max_length=10)
     station_name = models.CharField(max_length=25)
     district = models.CharField(max_length=15)

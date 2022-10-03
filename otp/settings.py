51269550a35376c750af28801b91c8c10f8b11e1
taksa <taksamir08@gmail.com>
2022-10-04 00:37:52 +0530

    Updated CORS

--- otp/settings.py
+++ otp/settings.py
@@ -94,6 +94,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173"

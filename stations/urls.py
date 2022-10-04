00dc4a5b4b1badbe5b49d2420c317706a94c542b
taksa <taksamir08@gmail.com>
2022-10-04 18:44:15 +0530

    Implemented Station Register

--- stations/urls.py
+++ stations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include, re_path
-from .views import Stationdata
+from .views import AddStation, GetStationNameById
 
 urlpatterns = [
-    re_path('filter/', Stationdata.as_view()),
+    re_path('add/', AddStation.as_view()),
+    re_path('get/', GetStationNameById.as_view()),
 ]
--- stations/views.py
+++ stations/views.py
@@ -1,18 +1,94 @@
+from asyncio.windows_events import NULL
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from url_filter.integrations.drf import DjangoFilterBackend
 from .models import stationModel
 from .StationSerialzer import StationSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
-class Stationdata(APIView):
-    pass
-    # def post(self, request):
-    #
-    #     if ('district' in request.data) and:
-    #
-    #         filtered_data  = Station.objects.filter(district=request.data['district'], subdivision=request.data['subdivision']).values()
-    #
-    #     return Response({filtered_data}, status=200)
+class AddStation(APIView):
+   permission_classes = [IsAuthenticated]
+
+   def post(self, request):
+    response = request.data
+
+    try:
+        stationId = response['station_id']
+
+        try:
+            stationModel.objects.get(station_id = stationId)
+
+            return Response(
+                status=status.HTTP_404_NOT_FOUND,
+                data={
+                    "success": "false",
+                    "message": "Station alredy exists"
+                    }
+            )
+
+        except:
+
+            x = stationModel.objects.create(
+                email = response["email"],
+                station_id = response["station_id"],
+                station_name = response["station_name"],
+                district = response["district"],
+                subdivision = response["subdivision"],
+                address = response["address"],
+                contact = response["contact"],
+                pincode = response["pincode"],
+            )
+            if (x == NULL):
+                return Response(
+                    status=status.HTTP_406_NOT_ACCEPTABLE,
+                    data={
+                        "success": "false",
+                        "message": "Oops! station didn't create successfully"
+                        }
+                    )
+            return Response(
+                    status=status.HTTP_200_OK,
+                    data={
+                        "success": "true",
+                        "message": "Station created successfully"
+                    }
+            )
+    except:
+        return Response(
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            data={
+                "success": "false",
+                "message": "Please provide correct details"
+            }
+        )
+
+class GetStationNameById(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request): 
+        response = request.data
+        station_id = response['station_id']
+
+        try:
+            stationName = stationModel.objects.get(station_id = station_id)
+            
+            return Response(
+                status=status.HTTP_200_OK,
+                data={
+                    "success": "true",
+                    "message": stationName.station_name
+                }
+            )
+        except:
+            return Response(
+                status=status.HTTP_200_OK,
+                data={
+                    "success": "false",
+                    "message": "Station id doesn't exist"
+                }
+            )
+
+        

6d235eb37bf40fe619fe07c72e94edbe968bf2a5
taksa <taksamir08@gmail.com>
2022-10-05 22:48:31 +0530

    Updated Serializers

--- otp/serializers.py
+++ otp/serializers.py
@@ -5,6 +5,7 @@ from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnico
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+from rest_framework.exceptions import AuthenticationFailed
 
 from otp.utils import Util
 
@@ -18,4 +19,31 @@ class ResetPasswordSerializer(serializers.Serializer):
     def validate(self, attrs):
         email=attrs['data'].get('email', '')
 
+        return super().validate(attrs)
+
+class NewPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(min_length=6, max_length=68, write_only=True)
+    token = serializers.CharField(min_length=1, write_only=True)
+    uidb64 = serializers.CharField(min_length=1, write_only=True)
+
+    class Meta:
+        fields=['password', 'token', 'uidb64']
+
+    def validate(self, attrs):
+        try:
+            password = attrs.get('password')
+            token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=id)
+
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise AuthenticationFailed('The reset link is invalid', 401)
+
+            user.set_password(password)
+            user.save()
+
+        except Exception as e:
+            raise AuthenticationFailed('The reset link is invalid', 401)
         return super().validate(attrs)

0839f9fcb40d7b0c9d6b2ce3dcc8de793e9b9767
taksa <taksamir08@gmail.com>
2022-10-05 22:51:59 +0530

    Added Utils

--- otp/utils.py
+++ otp/utils.py
@@ -1,10 +1,9 @@
 from django.core.mail import EmailMessage
-
+from django.conf import settings
 class Util:
     @staticmethod
     def send_email(data):
         email = EmailMessage(
-            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']]
+            subject=data['email_subject'], body=data['email_body'], from_email=settings.EMAIL_HOST_USER, to=[data['to_email']]
         )
-        print(email.send())
         email.send()

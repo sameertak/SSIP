91daed67218c9c733381f0aa84992131594bf27d
taksa <taksamir08@gmail.com>
2022-10-05 14:48:46 +0530

    Added Filter URL

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,6 +1,7 @@
-from .views import form
+from .views import FilterFeedback, form
 from django.urls import path
 
 urlpatterns = [
     path("form/", form.as_view(), name="Feedback Form"),
+    path("filter/", FilterFeedback.as_view(), name="Feedback Form"),
 ]

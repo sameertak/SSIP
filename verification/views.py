7718074a095cb7019609303e3e900b14ad15048c
taksa <taksamir08@gmail.com>
2022-10-05 14:55:45 +0530

    Cleaned the OTP Verification Views code

--- verification/views.py
+++ verification/views.py
@@ -1,6 +1,7 @@
 from _datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
 import pyotp
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import phoneModel
@@ -9,24 +10,20 @@ from twilio.rest import Client
 from decouple import config
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.utils import timezone
-# Create your models here.
 import django
 import geocoder
 
 
-# This class returns the string needed to generate the key
 class generateKey:
     @staticmethod
     def returnValue(phone):
         return str(phone) + str(datetime.date(datetime.now()))
 
-# Time after which OTP will expire
-EXPIRY_TIME = 50 # seconds
+EXPIRY_TIME = 50
 
 
 class getPhoneNumberRegistered_TimeBased(APIView):
     permission_classes = [AllowAny]
-    # Get to Create a call for OTP
     @staticmethod
     def get(request, phone):
         x_forw_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -39,20 +36,18 @@ class getPhoneNumberRegistered_TimeBased(APIView):
         lat_lng = add.latlng
 
         try:
-            mobile = phoneModel.objects.get(mobile=phone)  # if mobile already exists then take this else create New One
+            mobile = phoneModel.objects.get(mobile=phone)
         except ObjectDoesNotExist:
             phoneModel.objects.create(
                 mobile=phone,
-                ip_address=ip
+                ip_address=ip,
+                is_verified=False,
+                city=city,
+                lat_lng=lat_lng
             )
-            mobile = phoneModel.objects.get(mobile=phone)  # user Newly created Model
-
-
-        mobile.is_verified = False
-        mobile.city = city
-        mobile.lat_lng = lat_lng
+            mobile = phoneModel.objects.get(mobile=phone)
         mobile.counter += 1
-        mobile.save()  # Save the data
+        mobile.save()
 
         keygen = generateKey()
         key = base64.b32encode(keygen.returnValue(phone).encode())  # Key is generated
@@ -64,28 +59,47 @@ class getPhoneNumberRegistered_TimeBased(APIView):
 
         client = Client(account_sid, auth_token)
         phone_no = phoneModel.objects.get(mobile=phone)
-        message = client.messages.create(
+        client.messages.create(
             body=f'Your OTP is {OTP.now()}',
             from_=from_number,
             to=f'+91{phone_no.mobile}'
         )
 
-        # Using Multi-Threading send the OTP Using Messaging Services like Twilio or Fast2sms
-        return Response({"id": mobile.id}, status=200)  # Just for demonstration
+        return Response(
+            status=status.HTTP_200_OK,
+            data={
+                "id": mobile.id
+            }
+        )
 
-    # This Method verifies the OTP
     @staticmethod
     def post(request, phone):
         try:
             mobile = phoneModel.objects.get(mobile=phone)
         except ObjectDoesNotExist:
-            return Response("User does not exist", status=404)  # False Call
+            return Response(
+                status=status.HTTP_404_NOT_FOUND,
+                data={
+                    "User does not exist"
+                }
+            )
 
         keygen = generateKey()
-        key = base64.b32encode(keygen.returnValue(phone).encode())  # Generating Key
-        OTP = pyotp.TOTP(key,interval = EXPIRY_TIME)  # TOTP Model 
-        if OTP.verify(request.data["otp"]):  # Verifying the OTP
+        key = base64.b32encode(keygen.returnValue(phone).encode())
+        OTP = pyotp.TOTP(key,interval = EXPIRY_TIME)
+        if OTP.verify(request.data["otp"]):
             mobile.is_verified = True
             mobile.save()
-            return Response("You are authorised", status=200)
-        return Response("OTP is wrong/expired", status=400)
+            return Response(
+                status=status.HTTP_200_OK,
+                data={
+                    "You are authorised"
+                }
+            )
+
+        return Response(
+            status=status.HTTP_400_BAD_REQUEST,
+            data={
+                "OTP is Wrong/Expired"
+            }
+        )

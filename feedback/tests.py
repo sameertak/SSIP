182e98f6e53db9a615c0dfcecbf7ebd8b8e982ad
taksa <taksamir08@gmail.com>
2022-10-05 14:48:14 +0530

    Removed Test Code

--- feedback/tests.py
+++
@@ -1,5 +0,0 @@
-# # from django.test import TestCase
-# #
-# # # Create your tests here.
-# import os. import os,
-# print(“Current Working Directory ” , os. getcwd()) print(“Current Working Directory ” , os.getcwd())

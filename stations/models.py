59c871d3d746c0f365b1bbc403c45dffc28e2610
taksa <taksamir08@gmail.com>
2022-10-06 10:12:49 +0530

    Updated Model

--- stations/models.py
+++ stations/models.py
@@ -7,13 +7,13 @@ from django.conf import settings
 
 class stationModel(models.Model):
 
-    email = models.CharField(blank=True, max_length=20)
+    email = models.CharField(null=True, blank=True, max_length=20)
     station_id = models.CharField(max_length=10)
     station_name = models.CharField(max_length=25)
-    district = models.CharField(max_length=15)
+    district = models.CharField(null=True, max_length=15)
     subdivision = models.CharField(null=True, blank=True, max_length=10)
     address = models.CharField(default=' ', max_length=100)
-    contact = models.CharField(max_length=12)
+    contact = models.CharField(default='079-', max_length=12)
     pincode = models.CharField(max_length=6)
 
     def __str__(self):

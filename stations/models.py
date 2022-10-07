61cc8f3ca47b089ed825fb5ddfe3ad421ef9c1a4
taksa <taksamir08@gmail.com>
2022-10-08 02:15:52 +0530

    Updated Models

--- stations/models.py
+++ stations/models.py
@@ -25,6 +25,7 @@ class ControlroomModel(models.Model):
     district = models.CharField(max_length=15)
     contact = models.CharField(null=True, blank=True, max_length=20)
     email = models.EmailField(max_length=26)
+    district_id = models.CharField(default="",max_length=4)
 
     def __str__(self):
         return str(self.district)

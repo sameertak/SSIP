f4b6242b0764076502556680765d8a269c75b6c2
taksa <taksamir08@gmail.com>
2022-10-07 19:09:10 +0530

    CSV download API

--- feedback/views.py
+++ feedback/views.py
@@ -317,8 +317,8 @@ class GetTotalCountDistrictSubdivision(APIView):
         district = response['district']
 
         if district != "":
-            q = "SELECT feedback_responsemodel.id, COUNT(*) AS count FROM feedback_responsemodel INNER JOIN stations_stationmodel ON feedback_responsemodel.station_id = stations_stationmodel.station_id WHERE " \
-                "stations_stationmodel.district =" + "'" + district + "' GROUP BY stations_stationmodel.subdivision, feedback_responsemodel.id"
+            q = "SELECT stations_stationmodel.id, COUNT(*) AS count FROM feedback_responsemodel LEFT JOIN stations_stationmodel ON feedback_responsemodel.station_id = stations_stationmodel.station_id WHERE " \
+                "stations_stationmodel.district =" + "'" + district + "' GROUP BY stations_stationmodel.subdivision, stations_stationmodel.id"
             queryset = stationModel.objects.raw(q)
             serializer = SubdivisionCountSerializer(queryset, many=True)
             return Response(

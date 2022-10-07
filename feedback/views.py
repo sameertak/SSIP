d78b535e5764d34409271172f3845b9e976450c2
taksa <taksamir08@gmail.com>
2022-10-08 03:19:37 +0530

    Updated Views

--- feedback/views.py
+++ feedback/views.py
@@ -200,7 +200,7 @@ class FilterFeedback(APIView):
                 )
 
             if district == "" and subdivision == "" and rating == "" and station_id == "":
-                q = "SELECT * FROM feedback_responsemodel ORDER BY feedback_responsemodel.created_at DESC OFFSET '10' LIMIT '" + str(page) + "'"
+                q = f"SELECT * FROM feedback_responsemodel ORDER BY feedback_responsemodel.created_at DESC OFFSET 10 LIMIT {page}"
                 queryset = responseModel.objects.raw(q)
                 serializer = FeedbackSerializers(queryset, many=True)
                 count = len(serializer.data)

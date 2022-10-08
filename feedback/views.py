d482126991bd924ef7ca1da0b6a6b7379b6addb9
taksa <taksamir08@gmail.com>
2022-10-08 11:41:30 +0530

    Updated Filter

--- feedback/views.py
+++ feedback/views.py
@@ -550,7 +550,7 @@ class GetAvgDistrictSubdivision(APIView):
 
         if district != "":
             cursor = connection.cursor()
-            cursor.execute("SELECT s.subdivision, AVG(CAST(f.res4 as int)) AS count FROM feedback_responsemodel f INNER JOIN stations_stationmodel s ON f.station_id=s.station_id GROUP BY s.subdivision WHERE s.district='"+district+"'")
+            cursor.execute("SELECT s.subdivision, AVG(CAST(f.res4 as int)) AS count FROM feedback_responsemodel f INNER JOIN stations_stationmodel s ON f.station_id=s.station_id WHERE s.district='"+district+"' GROUP BY s.subdivision")
             row = cursor.description
             nt_result = namedtuple('Result', [col[0] for col in row])
             res = [nt_result(*row) for row in cursor.fetchall()]

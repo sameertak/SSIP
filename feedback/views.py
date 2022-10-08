0585939523d58683221438185f15fe0e4b5aef7d
taksa <taksamir08@gmail.com>
2022-10-08 06:44:56 +0530

    Updated Query

--- feedback/views.py
+++ feedback/views.py
@@ -524,7 +524,7 @@ class GetCountForEachRating(APIView):
 class GetAverageRatings(APIView):
     permission_classes = [AllowAny]
     def get(self, request):
-        q = "SELECT *, AVG(feedback_responsemodel.res4) AS count FROM feedback_responsemodel INNER JOIN stations_stationmodel ON feedback_responsemodel.station_id = stations_stationmodel.station_id GROUP BY stations_stationmodel.district"
+        q = "SELECT *, AVG( CAST(feedback_responsemodel.res4 as integer)) AS count FROM feedback_responsemodel INNER JOIN stations_stationmodel ON feedback_responsemodel.station_id = stations_stationmodel.station_id GROUP BY stations_stationmodel.district"
         queryset = stationModel.objects.raw(q)
 
         serializer = AvgRatingCountSerializer(queryset, many=True)
